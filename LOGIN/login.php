d6836035fb806a03415a31306d49b7640c89b5d1
Darshan Timbadiya <darshan.timbadiya125553@marwadiuniversity.ac.in>
2026-06-11 08:28:20 +0530

    11 -06

---
+++ LOGIN/login.php
@@ -0,0 +1,15 @@
+<!-- Create a Registration Form and a Login Form to collect user details and redirect the user from the Login Screen to the Home Screen after successful login. -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+        
+
+
+</body>
+</html>
